74c0fbe75b02fe87b86513b92632ea0f5ba78b46
leela Parameswara reddy <63552367+redminds@users.noreply.github.com>
2022-07-16 21:47:12 +0530

    Update with_key.sh

--- with_key.sh
+++ with_key.sh
@@ -9,6 +9,7 @@ PRE_UPLOAD=${INPUT_PRE_UPLOAD}
 if [ ! -z "$PRE_UPLOAD" ]; then
     { 
         echo "👌 Executing pre-upload script..." &&
+        echo ${INPUT_SSH_OPTIONS} ${INPUT_PORT} ${INPUT_USER}@${INPUT_HOST}  $INPUT_PRE_UPLOAD &&
         ssh ${INPUT_SSH_OPTIONS} -p ${INPUT_PORT} ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
         echo "✅ Executed pre-upload script"
     } || { 
