43975d5fcf2f4a498062238ea5542a4f32d02487
leela Parameswara reddy <63552367+redminds@users.noreply.github.com>
2022-07-16 21:44:19 +0530

    Update with_key.sh

--- with_key.sh
+++ with_key.sh
@@ -9,7 +9,7 @@ PRE_UPLOAD=${INPUT_PRE_UPLOAD}
 if [ ! -z "$PRE_UPLOAD" ]; then
     { 
         echo "👌 Executing pre-upload script..." &&
-        ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
+        ssh ${INPUT_SSH_OPTIONS} -p ${INPUT_PORT} ${INPUT_USER}@${INPUT_HOST} "$INPUT_PRE_UPLOAD && exit" &&
         echo "✅ Executed pre-upload script"
     } || { 
         echo "😢 Something went wrong during pre-upload script" && exit 1
@@ -18,7 +18,7 @@ fi
 
 {
     echo "🚚 Uploading via scp..." &&
-    scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P "${INPUT_PORT}" -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" && 
+    scp ${INPUT_SSH_OPTIONS} ${INPUT_SCP_OPTIONS} -P ${INPUT_PORT} -r ${INPUT_LOCAL} ${INPUT_USER}@${INPUT_HOST}:"${INPUT_REMOTE}" && 
     echo "🙌 Uploaded via scp"
 } || {
     echo "😢 Something went wrong during upload" && exit 1 
@@ -28,7 +28,7 @@ POST_UPLOAD=${INPUT_POST_UPLOAD}
 if [ ! -z "$POST_UPLOAD" ]; then
     {
         echo "👌 Executing post-upload script..." &&
-        ssh ${INPUT_SSH_OPTIONS} -p "${INPUT_PORT}" ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
+        ssh ${INPUT_SSH_OPTIONS} -p ${INPUT_PORT} ${INPUT_USER}@${INPUT_HOST} "$POST_UPLOAD && exit" &&
         echo "✅ Executed post-upload script"
     } || {
         echo "😢 Something went wrong during post-upload script" && exit 1
